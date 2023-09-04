ba6055682af9514cedb14bc4b338994f441fc386
mykytazviahin <mykytazviahin@gmail.com>
2023-09-04 17:22:10 +0300

    fix: refactored login custom validation

---
+++ src/auth/helpers/validateLogin.ts
@@ -0,0 +1,30 @@
+import { ExecutionContext, HttpStatus } from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
+import { LogInUserDto } from '../dtos/loginUser.dto';
+import { validate } from 'class-validator';
+import { Request, Response } from 'express';
+
+const validateLogin = async (context: ExecutionContext) => {
+  const request = context.switchToHttp().getRequest<Request>();
+  const response = context.switchToHttp().getResponse<Response>();
+
+  const body = plainToClass(LogInUserDto, request.body);
+
+  const errors = await validate(body);
+
+  const errorMessages = errors.flatMap(({ constraints }) =>
+    Object.values(constraints),
+  );
+
+  if (errorMessages.length > 0) {
+    // return bad request if validation fails
+
+    response.status(HttpStatus.BAD_REQUEST).send({
+      statusCode: HttpStatus.BAD_REQUEST,
+      error: 'Bad Request',
+      message: errorMessages,
+    });
+  }
+};
+
+export default validateLogin;

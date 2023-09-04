bd378e71e343c9612bfd2c4f549fa3ed9b2a984d
mykytazviahin <mykytazviahin@gmail.com>
2023-09-04 17:17:24 +0300

    feat: add custom validation for login endpoint

--- src/auth/guards/local-auth.guard.ts
+++ src/auth/guards/local-auth.guard.ts
@@ -1,5 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, HttpStatus, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { plainToClass } from 'class-transformer';
+import { LogInUserDto } from '../dtos/loginUser.dto';
+import { validate } from 'class-validator';
+import { Request, Response } from 'express';
 
 @Injectable()
-export class LocalAuthGuard extends AuthGuard('local') {}
+export class LocalAuthGuard extends AuthGuard('local') {
+  async canActivate(context: ExecutionContext) {
+    //dto validation before the guard execution
+    const request = context.switchToHttp().getRequest<Request>();
+    const response = context.switchToHttp().getResponse<Response>();
+
+    const body = plainToClass(LogInUserDto, request.body);
+
+    const errors = await validate(body);
+
+    const errorMessages = errors.flatMap(({ constraints }) =>
+      Object.values(constraints),
+    );
+
+    if (errorMessages.length > 0) {
+      // return bad request if validation fails
+
+      response.status(HttpStatus.BAD_REQUEST).send({
+        statusCode: HttpStatus.BAD_REQUEST,
+        error: 'Bad Request',
+        message: errorMessages,
+      });
+    }
+
+    return super.canActivate(context) as boolean | Promise<boolean>;
+  }
+}

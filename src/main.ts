cee72b480ae224a095acf60e86a3c8ff9f1f1575
mykytazviahin <mykytazviahin@gmail.com>
2023-09-22 23:09:04 +0300

    feat:enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ConfigService } from '@nestjs/config';
 import { swaggerConfig } from './config/swaggerConfig';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const configService = app.get(ConfigService);
   const port = configService.get<number>('port');
 
